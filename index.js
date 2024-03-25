553635629624fed4e0d9e8b0038331e40bd422e2
Zahira Khan <66169164+zahirakhan@users.noreply.github.com>
2024-03-25 07:20:41 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+#! /usr/bin/env node 
+import inquirer from "inquirer";
+import chalk from "chalk";
+const answer = await inquirer.prompt([
+    { message: chalk.blue("enter first number"), type: "number", name: "firstnumber" },
+    { message: chalk.blue("enter second number"), type: "number", name: "secondnumber" },
+    {
+        message: chalk.yellow("select one of these operator to perform action"),
+        type: "list",
+        name: "operator",
+        choices: ["addition", "subtraction", "multiplication", "division"],
+    },
+]);
+//conditional statement
+if (answer.operator === "addition") {
+    console.log(chalk.green(answer.firstnumber + answer.secondnumber));
+}
+else if (answer.operator === "subtraction") {
+    console.log(chalk.green(answer.firstnumber - answer.secondnumber));
+}
+else if (answer.operator === "multiplication") {
+    console.log(chalk.green(answer.firstnumber * answer.secondnumber));
+}
+else if (answer.operator === "division") {
+    console.log(chalk.green(answer.firstnumber / answer.secondnumber));
+}
+else {
+    console.log(chalk.red("Please select a valid operator"));
+}
